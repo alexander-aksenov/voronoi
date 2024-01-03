bd3117363cd8cb3d64588e80c6d43d96bd4645d8
Alexander Aksenov <alexander.aksenov@mail.ru>
2024-01-03 19:35:27 +0300

    point: add equality op and hash

--- point.h
+++ point.h
@@ -7,6 +7,21 @@ struct Point {
 
     Point() : x(0), y(0) {}
     Point(unsigned int x, unsigned int y) : x(x), y(y) {}
+
+    bool operator==(const Point& p) const
+    {
+        return (x == p.x) && (y == p.y);
+    }
 };
+
+template <>
+struct std::hash<Point> {
+    size_t operator()(const Point& p) const
+    {
+        return (std::hash<unsigned int>()(p.x)
+                ^ std::hash<unsigned int>()(p.y));
+    }
+};
+
 
 #endif /* POINT_H */
